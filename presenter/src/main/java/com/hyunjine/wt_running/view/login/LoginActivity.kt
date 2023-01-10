bed5c6ab8481a3c92b2bc5b33c1dbe45d24771ad
hyunjine <thevlakk1@gmail.com>
2023-01-11 01:20:30 +0900

    Feat: Set presenter module to app

---
+++ presenter/src/main/java/com/hyunjine/wt_running/view/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.hyunjine.wt_running.view.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hyunjine.wt_running.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
