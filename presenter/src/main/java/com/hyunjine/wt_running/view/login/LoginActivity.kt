763f80be9e6e996901bb2d38938ca46978f025ef
hyunjine <thevlakk1@gmail.com>
2023-01-11 01:28:48 +0900

    Feat: Set aac navigation on LoginActivity

--- presenter/src/main/java/com/hyunjine/wt_running/view/login/LoginActivity.kt
+++ presenter/src/main/java/com/hyunjine/wt_running/view/login/LoginActivity.kt
@@ -1,12 +1,12 @@
 package com.hyunjine.wt_running.view.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hyunjine.wt_running.R
+import com.hyunjine.wt_running.base.BaseActivity
+import com.hyunjine.wt_running.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login, R.id.nav_graph_login) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
     }
 }
